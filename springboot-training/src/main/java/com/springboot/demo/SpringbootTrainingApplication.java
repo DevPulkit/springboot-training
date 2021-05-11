28e82b7c867a2325787e9ea0860f12e3c36df462
DevPulkit <83303429+DevPulkit@users.noreply.github.com>
2021-05-11 07:48:15 +0530

    Add files via upload
    First commit

---
+++ springboot-training/src/main/java/com/springboot/demo/SpringbootTrainingApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTrainingApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
